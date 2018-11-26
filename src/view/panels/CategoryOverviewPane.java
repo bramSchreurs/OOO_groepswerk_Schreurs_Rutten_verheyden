08f39cdf863aff521dd6144d7eec2e1cd6769665
DESKTOP-UG7LS7L\Kasper <kasper.rutten@student.ucll.be>
2018-11-26 17:18:00 +0100

    Categories.txt toegevoegd

--- src/view/panels/CategoryOverviewPane.java
+++ src/view/panels/CategoryOverviewPane.java
@@ -15,12 +15,13 @@ import javafx.scene.layout.GridPane;
 public class CategoryOverviewPane extends GridPane {
 	private TableView table;
 	private Button btnNew;
+	private String name, description;
 	
 	public CategoryOverviewPane() {
 		this.setPadding(new Insets(5, 5, 5, 5));
         this.setVgap(5);
         this.setHgap(5);
-        
+
 		this.add(new Label("Categories:"), 0, 0, 1, 1);
 		
 		table = new TableView<>();
