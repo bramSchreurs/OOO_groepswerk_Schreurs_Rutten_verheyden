db2474c98dc14cb1e852b7b131746cf062735975
DESKTOP-UG7LS7L\Kasper <kasper.rutten@student.ucll.be>
2018-11-26 18:29:54 +0100

    textfiles toegevoegd, wat aitten proberen op de database

--- src/model/databank/Database.java
+++ src/model/databank/Database.java
@@ -1,4 +1,39 @@
 package model.databank;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Database {
+    private static String name, description, question, answer;
+    private static List<String> listCatName = new ArrayList<>();
+    private static List<String> listCatDesc = new ArrayList<>();
+    public Database() {
+
+    }
+    public static void main(String[] args){
+        Scanner scanCat = new Scanner("\\TextFiles\\Categories.txt");
+        scanCat.useDelimiter("::");
+        System.out.println(scanCat.nextLine());
+        /*while(scanCat.hasNextLine()) {
+            name = scanCat.next();
+            scanCat.delimiter();
+            description = scanCat.next();
+            listCatName.add(name);
+            listCatDesc.add(description);
+        }
+        System.out.println("All names:");
+        for (String n : listCatName){
+            System.out.println("\n" + n);
+        }
+        System.out.println("All descriptions:");
+        for (String d : listCatDesc){
+            System.out.println("\n" + d);
+        }*/
+
+
+    }
+
+
+
 }
