8aa0fcd8f51f2b0e1c03bcee365909d687bd8937
Redginald Verheyden <reddbz@hotmail.com>
2018-12-03 18:20:29 +0100

    interface databank
    GIT KANKER

--- src/model/FeedbackWijze.java
+++ src/model/FeedbackWijze.java
@@ -1,9 +1,18 @@
 package model;
 
 public enum FeedbackWijze {
-    SCORE,
-    SCOREENFEEDBACK,
-    FEEDBACK;
+    FEEDBACK("model.EvaluatieVormFeedback;"),
+    SCORE("model.EvaluatieVormScore;");
+    private String clasname;
 
+    public String getClasname() {
+        return clasname;
+    }
 
+    public void setClasname(String clasname) {
+        this.clasname = clasname;
+    }
+
+    FeedbackWijze(String classname){this.clasname=classname;} {
+    }
 }
---
+++ src/model/databank/Databanken.java
@@ -0,0 +1,4 @@
+package model.databank;
+
+public interface Databanken {
+}
