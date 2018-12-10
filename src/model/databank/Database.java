26acb1f4991e1970f05707a2d9d881812d6b2dac
Redginald Verheyden <reddbz@hotmail.com>
2018-12-10 17:35:48 +0100

    Databanken methodes gemaakt

--- src/model/databank/Database.java
+++ src/model/databank/Database.java
@@ -1,5 +1,9 @@
 package model.databank;
 
+import model.Categorie;
+import model.Test;
+import model.Vraag;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.SQLOutput;
@@ -75,4 +79,63 @@ public class Database implements Databanken{
 
     }
 
+    @Override
+    public void Addnewtest() {
+
+    }
+
+    @Override
+    public void AddnewCategorie() {
+
+    }
+
+    @Override
+    public void Addvraag() {
+
+    }
+
+    @Override
+    public void deletetest() {
+
+    }
+
+    @Override
+    public void deleteCategorie() {
+
+    }
+
+    @Override
+    public void deleteVraag() {
+
+    }
+
+    @Override
+    public ArrayList<Vraag> Scanallevragen() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Categorie> ScanalleCatogorien() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Test> ScanalleTesten() {
+        return null;
+    }
+
+    @Override
+    public void MaaknieuweTest() {
+
+    }
+
+    @Override
+    public void MaaknieuweCategorie() {
+
+    }
+
+    @Override
+    public void MaaknieuweVraag() {
+
+    }
 }
