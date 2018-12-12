9bdde9acb704aca2ce597a24b5883ed5563b82e0
Redginald Verheyden <reddbz@hotmail.com>
2018-12-12 20:47:14 +0100

    Databanken methodes gemaakt

--- src/model/databank/Databanken.java
+++ src/model/databank/Databanken.java
@@ -1,4 +1,5 @@
 package model.databank;
+//interface voor met databanken te werken
 
 import model.Categorie;
 import model.Test;
@@ -7,17 +8,29 @@ import model.Vraag;
 import java.util.ArrayList;
 
 public interface Databanken {
+    //nieuwe testin databank steken
     void Addnewtest();
+    // nieuwe Categorie in databank steken
     void AddnewCategorie();
+    //Nieuwe vraag in databank steken
     void Addvraag();
+    // test uit databank halen
     void deletetest();
+    //Categorie uit db halen
     void deleteCategorie();
+    //Vraag uit db halen
     void deleteVraag();
+    // Allen vragen in scannen
     ArrayList<Vraag>Scanallevragen();
+    //Allen Categorien in scannen
     ArrayList<Categorie>ScanalleCatogorien();
+    //Allen testen in scannen
     ArrayList<Test>ScanalleTesten();
+    //Nieuwe test maken
     void MaaknieuweTest();
+    //Nieuwe categorie maken
     void MaaknieuweCategorie();
+    //Nieuwe vraag maken
     void MaaknieuweVraag();
 
 }
