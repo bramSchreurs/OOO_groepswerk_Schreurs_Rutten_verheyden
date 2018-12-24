72bb7251cc9c78436ab63cfec90177ecaabeaa70
bramSchreurs <bram.schreurs@student.ucll.be>
2018-12-24 14:36:33 +0100

    test afleggen wekt zonder DB

--- src/view/panels/MessagePane.java
+++ src/view/panels/MessagePane.java
@@ -2,6 +2,7 @@ package view.panels;
 
 import java.util.Observer;
 
+import controller.SelfTestController;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
@@ -14,6 +15,7 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import model.databank.DatabaseWithtxt;
 
 public class MessagePane extends GridPane {
 	private Button testButton;
@@ -27,14 +29,13 @@ public class MessagePane extends GridPane {
         this.setHgap(5);
         
 		testButton = new Button("Evaluate");
-		testButton.setOnAction(new EventHandler<ActionEvent>() { //TODO remove or generalize
-			
-			@Override
-			public void handle(ActionEvent event) {
-			}
-		});
+		testButton.setOnAction(event -> this.createTestPane());
 		add(testButton, 0,1,1,1);
 		setHalignment(testButton, HPos.CENTER);
+	}
+
+	private void createTestPane() {
+		new TestPane(new SelfTestController(new DatabaseWithtxt()));
 	}
 
 }
