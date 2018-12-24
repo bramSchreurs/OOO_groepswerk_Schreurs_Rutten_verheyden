5bd0178a25ddd465986c95e431b023580cf1b933
Redginald Verheyden <reddbz@hotmail.com>
2018-12-24 13:41:16 +0100

    facade factory gefixt enums gedaan

--- src/model/TestFacade.java
+++ src/model/TestFacade.java
@@ -5,10 +5,85 @@ import java.util.ArrayList;
 public class TestFacade {
     // Vragen methodes
      //Maak nieuwe vraag aan
+    //TODO setters
+    //TODO setters
     public  Vraag Createvraag(String vraagString, ArrayList correcteAntwoorden, ArrayList mogelijkeAntwoorden, String feedback,Categorie categorie, int score){
         return new Vraag(vraagString,correcteAntwoorden, mogelijkeAntwoorden,feedback,categorie,score);
     }
+    public Categorie VraaggetCategorie(Vraag vraag) {
+        return vraag.categorie;
+    }
 
+    public void VraagsetCategorie(Vraag vraag,Categorie categorie)
+    {
+       vraag.setCategorie(categorie);
+    }
+
+    public int VraagsgetVragenId(Vraag vraag)
+    {
+        return vraag.getVragenId();
+    }
+
+    public void VraagssetVragenId(Vraag vraag)
+    {
+        vraag.setVragenId();
+    }
+
+    public String VraagsgetVraagString(Vraag vraag)
+    {
+        return vraag.getVraagString();
+    }
+
+    public void VraagssetVraagString(Vraag vraag,String vraags)
+    {
+        vraag.setVraagString(vraags);
+    }
+
+    public ArrayList VraagsgetCorrecteAntwoorden(Vraag vraag)
+    {
+        return vraag.getCorrecteAntwoorden();
+    }
+
+    public void VraagssetCorrecteAntwoorden(Vraag vraag)
+    {
+        vraag.setCorrecteAntwoorden();
+    }
+
+    public ArrayList VraagsgetMogelijkeAntwoorden(Vraag vraag)
+    {
+        return vraag.getMogelijkeAntwoorden();
+    }
+
+    public void VraagssetGegevenAntwoorden(Vraag vraag)
+    {
+        vraag.setGegevenAntwoorden();
+    }
+
+    public String VraagsgetFeedback(Vraag vraag)
+    {
+        return vraag.getFeedback();
+    }
+
+    public void VraagssetFeedback(Vraag vraag,String feedback)
+    {
+        vraag.setFeedback(feedback);
+    }
+
+    public int VraagsgetScore(Vraag vraag)
+    {
+        return vraag.getScore();
+    }
+
+    public void VraagssetScore(Vraag vraag,int score)
+    {
+        vraag.setScore(score);
+    }
+
+    public void VraagsaddVraagToDatabase(Vraag vraag)
+    {
+        vraag.addVraagToDatabase();
+
+    }
     //Test methodes
         //Testen maken
    public Test createTest(String naam){
